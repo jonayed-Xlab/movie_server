88dd93a9f246f87ee1f2f6be7ca4cc3ea85b0df3
jonayed-Xlab <jonayed153gmail.com>
2022-08-19 18:21:37 +0600

    com

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, items := range movies {
+		if items.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(1000000))
+			movies = append(movies, movie)
+		}
+	}
+
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, items := range movies {
+		if items.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "4812", Title: "Muxauim", Director: &Director{FirstName: "Alex", LastName: "Hun"}})
+
+	r.HandleFunc("/Movies", getMovies).Methods("GET")
+	r.HandleFunc("/Movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/Movies", createMovie).Methods("POST")
+	r.HandleFunc("/Movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/Movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting Server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
